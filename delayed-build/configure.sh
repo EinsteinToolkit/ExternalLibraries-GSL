e887ea13a3ff58d354cc5cf67e71abfb84f0352e
eschnett <eschnett@9e23cc15-e5c8-4d65-9080-beda90ea645b>
2014-11-25 00:54:22 +0000

    Create branch
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/GSL/branches/eschnett@52 9e23cc15-e5c8-4d65-9080-beda90ea645b

---
+++ delayed-build/configure.sh
@@ -0,0 +1,191 @@
+#! /bin/bash
+
+################################################################################
+# Prepare
+################################################################################
+
+# Set up shell
+if [ "$(echo ${VERBOSE} | tr '[:upper:]' '[:lower:]')" = 'yes' ]; then
+    set -x                      # Output commands
+fi
+set -e                          # Abort on errors
+
+
+
+################################################################################
+# Search
+################################################################################
+
+if [ -z "${GSL_DIR}" ]; then
+    echo "BEGIN MESSAGE"
+    echo "GSL selected, but GSL_DIR not set. Checking some places..."
+    echo "END MESSAGE"
+    
+    FILES="include/gsl/gsl_math.h"
+    DIRS="/usr /usr/local /usr/local/gsl /usr/local/packages/gsl /usr/local/apps/gsl ${HOME} c:/packages/gsl"
+    for dir in $DIRS; do
+        GSL_DIR="$dir"
+        for file in $FILES; do
+            if [ ! -r "$dir/$file" ]; then
+                unset GSL_DIR
+                break
+            fi
+        done
+        if [ -n "$GSL_DIR" ]; then
+            break
+        fi
+    done
+    
+    if [ -z "$GSL_DIR" ]; then
+        echo "BEGIN MESSAGE"
+        echo "GSL not found"
+        echo "END MESSAGE"
+    else
+        echo "BEGIN MESSAGE"
+        echo "Found GSL in ${GSL_DIR}"
+        echo "END MESSAGE"
+    fi
+fi
+
+
+
+################################################################################
+# Build
+################################################################################
+
+if [ -z "${GSL_DIR}"                                            \
+     -o "$(echo "${GSL_DIR}" | tr '[a-z]' '[A-Z]')" = 'BUILD' ]
+then
+    echo "BEGIN MESSAGE"
+    echo "Using bundled GSL..."
+    echo "END MESSAGE"
+    
+    # check for required tools. Do this here so that we don't require them when
+    # using the system library
+    if [ x$TAR = x ] ; then
+      echo 'BEGIN ERROR'
+      echo 'Could not find tar command. Please make sure that (gnu) tar is present'
+      echo 'and that the TAR variable is set to its location.'
+      echo 'END ERROR'
+      exit 1
+    fi
+    #if [ x$PATCH = x ] ; then
+    #  echo 'BEGIN ERROR'
+    #  echo 'Could not find patch command. Please make sure that (gnu) tar is present'
+    #  echo 'and that the PATCH variable is set to its location.'
+    #  echo 'END ERROR'
+    #  exit 1
+    #fi
+
+    # Set locations
+    THORN=GSL
+    NAME=gsl-1.16
+    SRCDIR="$(dirname $0)"
+    BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
+    if [ -z "${GSL_INSTALL_DIR}" ]; then
+        INSTALL_DIR=${SCRATCH_BUILD}/external/${THORN}
+    else
+        echo "BEGIN MESSAGE"
+        echo "Installing GSL into ${GSL_INSTALL_DIR} "
+        echo "END MESSAGE"
+        INSTALL_DIR=${GSL_INSTALL_DIR}
+    fi
+    DONE_FILE=${SCRATCH_BUILD}/done/${THORN}
+    GSL_DIR=${INSTALL_DIR}
+    
+    if [ -e ${DONE_FILE} -a ${DONE_FILE} -nt ${SRCDIR}/dist/${NAME}.tar.gz \
+                         -a ${DONE_FILE} -nt ${SRCDIR}/configure.sh ]
+    then
+        echo "BEGIN MESSAGE"
+        echo "GSL has already been built; doing nothing"
+        echo "END MESSAGE"
+    else
+        echo "BEGIN MESSAGE"
+        echo "Building GSL"
+        echo "END MESSAGE"
+        
+        # Build in a subshell
+        (
+        exec >&2                    # Redirect stdout to stderr
+        if [ "$(echo ${VERBOSE} | tr '[:upper:]' '[:lower:]')" = 'yes' ]; then
+            set -x                  # Output commands
+        fi
+        set -e                      # Abort on errors
+        cd ${SCRATCH_BUILD}
+        
+        # Set up environment
+        unset LIBS
+        if echo '' ${ARFLAGS} | grep 64 > /dev/null 2>&1; then
+            export OBJECT_MODE=64
+        fi
+        
+        echo "GSL: Preparing directory structure..."
+        mkdir build external done 2> /dev/null || true
+        rm -rf ${BUILD_DIR} ${INSTALL_DIR}
+        mkdir ${BUILD_DIR} ${INSTALL_DIR}
+        
+        echo "GSL: Unpacking archive..."
+        pushd ${BUILD_DIR}
+        ${TAR?} xzf ${SRCDIR}/dist/${NAME}.tar.gz
+        
+        echo "GSL: Configuring..."
+        cd ${NAME}
+        ./configure --prefix=${GSL_DIR} --enable-shared=no
+        
+        echo "GSL: Building..."
+        ${MAKE}
+        
+        echo "GSL: Installing..."
+        ${MAKE} install
+        popd
+        
+        echo "GSL: Cleaning up..."
+        rm -rf ${BUILD_DIR}
+        
+        date > ${DONE_FILE}
+        echo "GSL: Done."
+        )
+        if (( $? )); then
+            echo 'BEGIN ERROR'
+            echo 'Error while building GSL. Aborting.'
+            echo 'END ERROR'
+            exit 1
+        fi
+    fi
+    
+fi
+
+
+
+################################################################################
+# Configure Cactus
+################################################################################
+
+# Set options
+if [ -x ${GSL_DIR}/bin/gsl-config ]; then
+    inc_dirs="$(${GSL_DIR}/bin/gsl-config --cflags)"
+    lib_dirs="$(${GSL_DIR}/bin/gsl-config --libs)"
+    libs="$(${GSL_DIR}/bin/gsl-config --libs)"
+    # Translate option flags into Cactus options:
+    # - for INC_DIRS, remove -I prefix from flags
+    # - for LIB_DIRS, remove all -l flags, and remove -L prefix from flags
+    # - for LIBS, keep only -l flags, and remove -l prefix from flags
+    GSL_INC_DIRS="$(echo '' $(for flag in $inc_dirs; do echo '' $flag; done | sed -e 's/^ -I//'))"
+    GSL_LIB_DIRS="$(echo '' $(for flag in $lib_dirs; do echo '' $flag; done | grep -v '^ -l' | sed -e 's/^ -L//'))"
+    GSL_LIBS="$(echo '' $(for flag in $libs; do echo '' $flag; done | grep '^ -l' | sed -e 's/^ -l//'))"
+fi
+
+GSL_INC_DIRS="$(${CCTK_HOME}/lib/sbin/strip-incdirs.sh ${GSL_INC_DIRS})"
+GSL_LIB_DIRS="$(${CCTK_HOME}/lib/sbin/strip-libdirs.sh ${GSL_LIB_DIRS})"
+
+# Pass options to Cactus
+echo "BEGIN MAKE_DEFINITION"
+echo "GSL_DIR      = ${GSL_DIR}"
+echo "GSL_INC_DIRS = ${GSL_INC_DIRS}"
+echo "GSL_LIB_DIRS = ${GSL_LIB_DIRS}"
+echo "GSL_LIBS     = ${GSL_LIBS}"
+echo "END MAKE_DEFINITION"
+
+echo 'INCLUDE_DIRECTORY $(GSL_INC_DIRS)'
+echo 'LIBRARY_DIRECTORY $(GSL_LIB_DIRS)'
+echo 'LIBRARY           $(GSL_LIBS)'
