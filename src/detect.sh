aaafabaa7a3a411ca6ca6a08fa6bbe90ae381e42
eschnett <eschnett@9e23cc15-e5c8-4d65-9080-beda90ea645b>
2014-12-01 19:48:51 +0000

    Merge eschnett/delayed-build
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/GSL/trunk@64 9e23cc15-e5c8-4d65-9080-beda90ea645b

--- src/detect.sh
+++ src/detect.sh
@@ -60,27 +60,27 @@ then
     echo "Using bundled GSL..."
     echo "END MESSAGE"
     
-    # check for required tools. Do this here so that we don't require them when
-    # using the system library
-    if [ x$TAR = x ] ; then
-      echo 'BEGIN ERROR'
-      echo 'Could not find tar command. Please make sure that (gnu) tar is present'
-      echo 'and that the TAR variable is set to its location.'
-      echo 'END ERROR'
-      exit 1
+    # Check for required tools. Do this here so that we don't require
+    # them when using the system library.
+    if [ "x$TAR" = x ] ; then
+        echo 'BEGIN ERROR'
+        echo 'Could not find tar command.'
+        echo 'Please make sure that the (GNU) tar command  is present,'
+        echo 'and that the TAR variable is set to its location.'
+        echo 'END ERROR'
+        exit 1
     fi
-    #if [ x$PATCH = x ] ; then
-    #  echo 'BEGIN ERROR'
-    #  echo 'Could not find patch command. Please make sure that (gnu) tar is present'
-    #  echo 'and that the PATCH variable is set to its location.'
-    #  echo 'END ERROR'
-    #  exit 1
-    #fi
+    if [ "x$PATCH" = x ] ; then
+        echo 'BEGIN ERROR'
+        echo 'Could not find patch command.'
+        echo 'Please make sure that the patch command is present,'
+        echo 'and that the PATCH variable is set to its location.'
+        echo 'END ERROR'
+        exit 1
+    fi
 
     # Set locations
     THORN=GSL
-    NAME=gsl-1.16
-    SRCDIR="$(dirname $0)"
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
     if [ -z "${GSL_INSTALL_DIR}" ]; then
         INSTALL_DIR=${SCRATCH_BUILD}/external/${THORN}
@@ -90,69 +90,15 @@ then
         echo "END MESSAGE"
         INSTALL_DIR=${GSL_INSTALL_DIR}
     fi
-    DONE_FILE=${SCRATCH_BUILD}/done/${THORN}
     GSL_DIR=${INSTALL_DIR}
-    
-    if [ -e ${DONE_FILE} -a ${DONE_FILE} -nt ${SRCDIR}/dist/${NAME}.tar.gz \
-                         -a ${DONE_FILE} -nt ${SRCDIR}/configure.sh ]
-    then
-        echo "BEGIN MESSAGE"
-        echo "GSL has already been built; doing nothing"
-        echo "END MESSAGE"
-    else
-        echo "BEGIN MESSAGE"
-        echo "Building GSL"
-        echo "END MESSAGE"
-        
-        # Build in a subshell
-        (
-        exec >&2                    # Redirect stdout to stderr
-        if [ "$(echo ${VERBOSE} | tr '[:upper:]' '[:lower:]')" = 'yes' ]; then
-            set -x                  # Output commands
-        fi
-        set -e                      # Abort on errors
-        cd ${SCRATCH_BUILD}
-        
-        # Set up environment
-        unset LIBS
-        if echo '' ${ARFLAGS} | grep 64 > /dev/null 2>&1; then
-            export OBJECT_MODE=64
-        fi
-        
-        echo "GSL: Preparing directory structure..."
-        mkdir build external done 2> /dev/null || true
-        rm -rf ${BUILD_DIR} ${INSTALL_DIR}
-        mkdir ${BUILD_DIR} ${INSTALL_DIR}
-        
-        echo "GSL: Unpacking archive..."
-        pushd ${BUILD_DIR}
-        ${TAR?} xzf ${SRCDIR}/dist/${NAME}.tar.gz
-        
-        echo "GSL: Configuring..."
-        cd ${NAME}
-        ./configure --prefix=${GSL_DIR} --enable-shared=no
-        
-        echo "GSL: Building..."
-        ${MAKE}
-        
-        echo "GSL: Installing..."
-        ${MAKE} install
-        popd
-        
-        echo "GSL: Cleaning up..."
-        rm -rf ${BUILD_DIR}
-        
-        date > ${DONE_FILE}
-        echo "GSL: Done."
-        )
-        if (( $? )); then
-            echo 'BEGIN ERROR'
-            echo 'Error while building GSL. Aborting.'
-            echo 'END ERROR'
-            exit 1
-        fi
-    fi
-    
+    GSL_INC_DIRS="$GSL_DIR/include"
+    GSL_LIB_DIRS="$GSL_DIR/lib"
+    GSL_LIBS="gsl gslcblas"
+else
+    THORN=GSL
+    DONE_FILE=${SCRATCH_BUILD}/done/${THORN}
+    mkdir ${SCRATCH_BUILD}/done 2> /dev/null || true
+    date > ${DONE_FILE}
 fi
 
 
@@ -160,6 +106,11 @@ fi
 ################################################################################
 # Configure Cactus
 ################################################################################
+
+# Pass configuration options to build script
+echo "BEGIN MAKE_DEFINITION"
+echo "GSL_INSTALL_DIR = ${GSL_INSTALL_DIR}"
+echo "END MAKE_DEFINITION"
 
 # Set options
 if [ -x ${GSL_DIR}/bin/gsl-config ]; then
